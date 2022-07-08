929c5f78a49cf3a0c53f402d9ab9c13a4039a16f
Andrii Prokhorovych <prohorovychua@gmail.com>
2022-07-08 11:23:11 +0300

    feat: add walletsolidity api calls

--- pkg/getBlockByNum.go
+++ pkg/getBlockByNum.go
@@ -15,10 +15,38 @@ type GetBlockByNumResp struct {
 			TxTrieRoot     string `json:"txTrieRoot"`
 			WitnessAddress string `json:"witness_address"`
 			ParentHash     string `json:"parentHash"`
+			Version        int    `json:"version"`
 			Timestamp      int64  `json:"timestamp"`
 		} `json:"raw_data"`
 		WitnessSignature string `json:"witness_signature"`
 	} `json:"block_header"`
+	Transactions []struct {
+		Ret []struct {
+			ContractRet string `json:"contractRet"`
+		} `json:"ret"`
+		Signature []string `json:"signature"`
+		TxID      string   `json:"txID"`
+		RawData   struct {
+			Contract []struct {
+				Parameter struct {
+					Value struct {
+						Data            string `json:"data"`
+						OwnerAddress    string `json:"owner_address"`
+						ContractAddress string `json:"contract_address"`
+						CallValue       int    `json:"call_value,omitempty"`
+					} `json:"value"`
+					TypeUrl string `json:"type_url"`
+				} `json:"parameter"`
+				Type string `json:"type"`
+			} `json:"contract"`
+			RefBlockBytes string `json:"ref_block_bytes"`
+			RefBlockHash  string `json:"ref_block_hash"`
+			Expiration    int64  `json:"expiration"`
+			FeeLimit      int    `json:"fee_limit,omitempty"`
+			Timestamp     int64  `json:"timestamp"`
+		} `json:"raw_data"`
+		RawDataHex string `json:"raw_data_hex"`
+	} `json:"transactions"`
 }
 
 func (a *Api) GetBlockByNum(number int32) *GetBlockByNumResp {
@@ -27,7 +55,7 @@ func (a *Api) GetBlockByNum(number int32) *GetBlockByNumResp {
 	})
 	responseBody := bytes.NewBuffer(postBody)
 
-	res, err := http.Post(fmt.Sprintf("%s/wallet/getblockbynum", a.endpoint), "application/json", responseBody)
+	res, err := http.Post(fmt.Sprintf("%s/walletsolidity/getblockbynum", a.endpoint), "application/json", responseBody)
 	defer res.Body.Close()
 
 	if err != nil {
---
+++ pkg/getTransactionInfoById.go
@@ -0,0 +1,49 @@
+package pkg
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type GetTransactionInfoByIdResp struct {
+	Id              string   `json:"id"`
+	Fee             int      `json:"fee"`
+	BlockNumber     int      `json:"blockNumber"`
+	BlockTimeStamp  int64    `json:"blockTimeStamp"`
+	ContractResult  []string `json:"contractResult"`
+	ContractAddress string   `json:"contract_address"`
+	Receipt         struct {
+		OriginEnergyUsage int    `json:"origin_energy_usage"`
+		EnergyUsageTotal  int    `json:"energy_usage_total"`
+		NetFee            int    `json:"net_fee"`
+		Result            string `json:"result"`
+	} `json:"receipt"`
+	Log []struct {
+		Address string   `json:"address"`
+		Topics  []string `json:"topics"`
+		Data    string   `json:"data"`
+	} `json:"log"`
+}
+
+func (a *Api) GetTransactionInfoById(id string) *GetTransactionInfoByIdResp {
+	postBody, _ := json.Marshal(map[string]interface{}{
+		"value": id,
+	})
+	responseBody := bytes.NewBuffer(postBody)
+
+	res, err := http.Post(fmt.Sprintf("%s/walletsolidity/gettransactioninfobyid", a.endpoint), "application/json", responseBody)
+	defer res.Body.Close()
+
+	if err != nil {
+		a.log.Error(err.Error())
+		return &GetTransactionInfoByIdResp{}
+	}
+
+	var data GetTransactionInfoByIdResp
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&data)
+
+	return &data
+}
