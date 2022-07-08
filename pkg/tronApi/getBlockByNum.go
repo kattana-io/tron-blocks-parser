17282ca793effe9de214dfc5d35ccf57f0ebca69
Andrii Prokhorovych <prohorovychua@gmail.com>
2022-07-08 11:36:37 +0300

    refactor: move transaction to separate struct

--- pkg/tronApi/getBlockByNum.go
+++ pkg/tronApi/getBlockByNum.go
@@ -20,33 +20,35 @@ type GetBlockByNumResp struct {
 		} `json:"raw_data"`
 		WitnessSignature string `json:"witness_signature"`
 	} `json:"block_header"`
-	Transactions []struct {
-		Ret []struct {
-			ContractRet string `json:"contractRet"`
-		} `json:"ret"`
-		Signature []string `json:"signature"`
-		TxID      string   `json:"txID"`
-		RawData   struct {
-			Contract []struct {
-				Parameter struct {
-					Value struct {
-						Data            string `json:"data"`
-						OwnerAddress    string `json:"owner_address"`
-						ContractAddress string `json:"contract_address"`
-						CallValue       int    `json:"call_value,omitempty"`
-					} `json:"value"`
-					TypeUrl string `json:"type_url"`
-				} `json:"parameter"`
-				Type string `json:"type"`
-			} `json:"contract"`
-			RefBlockBytes string `json:"ref_block_bytes"`
-			RefBlockHash  string `json:"ref_block_hash"`
-			Expiration    int64  `json:"expiration"`
-			FeeLimit      int    `json:"fee_limit,omitempty"`
-			Timestamp     int64  `json:"timestamp"`
-		} `json:"raw_data"`
-		RawDataHex string `json:"raw_data_hex"`
-	} `json:"transactions"`
+	Transactions []Transaction `json:"transactions"`
+}
+
+type Transaction struct {
+	Ret []struct {
+		ContractRet string `json:"contractRet"`
+	} `json:"ret"`
+	Signature []string `json:"signature"`
+	TxID      string   `json:"txID"`
+	RawData   struct {
+		Contract []struct {
+			Parameter struct {
+				Value struct {
+					Data            string `json:"data"`
+					OwnerAddress    string `json:"owner_address"`
+					ContractAddress string `json:"contract_address"`
+					CallValue       int    `json:"call_value,omitempty"`
+				} `json:"value"`
+				TypeUrl string `json:"type_url"`
+			} `json:"parameter"`
+			Type string `json:"type"`
+		} `json:"contract"`
+		RefBlockBytes string `json:"ref_block_bytes"`
+		RefBlockHash  string `json:"ref_block_hash"`
+		Expiration    int64  `json:"expiration"`
+		FeeLimit      int    `json:"fee_limit,omitempty"`
+		Timestamp     int64  `json:"timestamp"`
+	} `json:"raw_data"`
+	RawDataHex string `json:"raw_data_hex"`
 }
 
 func (a *Api) GetBlockByNum(number int32) *GetBlockByNumResp {
